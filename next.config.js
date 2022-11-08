4df4869136951b244329d999a90714673d2f71ba
Alexis Rossi <arossi@FLX-ARROS-L0092.local>
2022-11-08 16:43:34 -0500

    webpack configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,39 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  /* config options here */
+  webpack: (config, options) => {
+    const { ModuleFederationPlugin } = options.webpack.container;
+    const HtmlWebpackPlugin = require("html-webpack-plugin");
+    if (!options.isServer) {
+      config.plugins.push(
+        new ModuleFederationPlugin({
+          name: "pepe",
+          // library: {
+          //   type: "var",
+          //   name: "Loyalty",
+          // },
+          filename: "static/chunks/remoteEntry.js",
+          exposes: {
+            "./Loyalty": "./src/components/Loyalty/index",
+          },
+          remotes: {},
+          shared: [],
+        })
+      );
 
-module.exports = nextConfig
+      config.cache = false;
+
+      config.output.publicPath = "http://localhost:3502/_next/";
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      });
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/components/Loyalty/index.tsx
@@ -0,0 +1,9 @@
+const Loyalty = () => {
+  return (
+    <div>
+      <h1>Loyalttyyyyy</h1>
+    </div>
+  );
+};
+
+export default Loyalty;
